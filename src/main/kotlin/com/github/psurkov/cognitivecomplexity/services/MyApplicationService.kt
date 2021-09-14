d33cfebcf682821c9529f0edccdb7a63e8241cff
GitHub Action <action@github.com>
2021-09-14 18:46:42 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/psurkov/cognitivecomplexity/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.psurkov.cognitivecomplexity.services
+
+import com.github.psurkov.cognitivecomplexity.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
